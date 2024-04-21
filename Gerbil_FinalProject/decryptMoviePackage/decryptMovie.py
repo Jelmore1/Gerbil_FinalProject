e36cc3b71605a615602e5b58875ac8e22e605a04
walsh <walsh@LAPTOP-J239KI4O.lan>
2024-04-21 13:51:02 -0400

    updated commit of decryptMoviePackage.

--- Gerbil_FinalProject/decryptMoviePackage/decryptMovie.py
+++ Gerbil_FinalProject/decryptMoviePackage/decryptMovie.py
@@ -1,26 +1,49 @@
-'''
-Created on Apr 18, 2024
+# Name: Connor Walsh
+# email: walsh2ct@mail.uc.edu
+# Assignment Number: Final Project
+# Due Date: April 23, 2024
+# Course/Section: IS4010-001
+# Semester/Year: Spring 2024
+# Brief Description of the assignment: This is our final project based in encryption/decryption
 
-@author: walsh
-'''
+# Brief Description of what this module does. This module creates three functions that reads an encrypted message, decrypts that message, then prints the results.
+# Citations:
+# Anything else that's relevant:
 import json
 from cryptography.fernet import Fernet
 
-def read_encrypted_message(file_path, team_name):
+def readEncryptedMessage(file_path, teamName):
+    '''
+    Reads the encrypted message for the specified team from the JSON file.
+    @param
+        file_path: The path to the JSON file containing encrypted messages.
+        team_name: The name of the team whose encrypted message is to be retrieved.
+    @returns The encrypted message for our team Gerbil
+    '''
     with open(file_path, "r") as file:
         data = json.load(file)
-        return data.get(team_name)
+        return data.get(teamName)
 
-def decrypt_message(encrypted_message, key):
+def decryptMessage(encryptedMessage, key):
+    '''
+    Decrypts the encrypted message using the provided key.
+    @param
+        encryptedMessage: The encrypted message to be decrypted.
+        key: The decryption key.
+    @return str: The decrypted message
+    '''
     fernet = Fernet(key)
-    decrypted_message = fernet.decrypt(encrypted_message.encode()).decode()
-    return decrypted_message
+    decryptedMessage = fernet.decrypt(encryptedMessage.encode()).decode()
+    return decryptedMessage
 
 def findMovieName():
+    '''
+    Main function that prints the decrypted message
+    '''
     file_path = 'TeamsAndEncryptedMessagesForDistribution - 001.json'
-    team_name = "Gerbil"
+    teamName = "Gerbil"
     key = b'8viv7QrLsAKgvcV3JsZkKjZwQiinhtPQhUvbS2B14LM='
 
-    encrypted_message = read_encrypted_message(file_path, team_name)
-    decrypted_message = decrypt_message(encrypted_message[0], key)
-    print("Our Movie:", decrypted_message)
+    encryptedMessage = readEncryptedMessage(file_path, teamName)
+    decryptedMessage = decryptMessage(encryptedMessage[0], key)
+    print("Our Movie:", decryptedMessage)
